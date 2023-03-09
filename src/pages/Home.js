e7f4a9ebea9ce53abcbd95711fb206415dacc750
juliovianadev <juliovianadev@gmail.com>
2023-03-09 14:01:42 -0300

    use navigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom'
 function HomePage() {
+  const navigate = useNavigate('/products')
+
+  const cliquei = ()=>{
+    navigate('/products')
+  }
   return (
-    <h1>HomePage</h1>
+    <>
+      <h1>HomePage</h1>
+      <button onClick={cliquei}>Navegar</button>
+    </>
   )
 }
 
